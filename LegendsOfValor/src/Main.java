7d1bc0438fcd25fb02dff8cd9ea629ce7536107f
Ming-Han Hsieh <jeff0987@bu.edu>
2022-11-18 11:39:21 -0500

    I deleted lines

--- LegendsOfValor/src/Main.java
+++ LegendsOfValor/src/Main.java
@@ -1,9 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Goodbye!");
-        System.out.println("Hi!");
-        System.out.println("Hey there");
-
     }
 }
