26f14b358faa13b228ee2490ac340f7521c72d8d
Spandana Patil <spandanapatil@Spandanas-MacBook-Pro.local>
2022-11-22 17:48:52 -0500

    update

--- LegendsOfValor/src/Main.java
+++ LegendsOfValor/src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Bye!");
+        System.out.println("No");
+        System.out.println("Yes");
         Random_map map = new Random_map();
         map.initial_map();
         map.print_random_map_1();
